bc7f290381d01014ba4220ba4ff469a7a502fe61
Vedant Pandey <kalashnikov@Vedants-Laptop.local>
2024-07-29 22:22:59 +0530

    test commit

--- next-server/src/app/layout.tsx
+++ next-server/src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +21,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    // <html lang="en">
+    //   <body className={inter.className}>
+        <ClerkProvider>
+        <html lang='en'>
+      <body>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        {children}
+      </body>
     </html>
+        </ClerkProvider>
+    // </body>
+    // </html>
   );
 }
---
+++ next-server/src/components/header.tsx
@@ -0,0 +1,30 @@
+import { UserButton,SignOutButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import { auth } from '@clerk/nextjs/server';
+
+export default async function Header() {
+//   const { userId } = auth();
+const { userId } : { userId: string | null } = auth();
+
+
+  return (
+    <div className='bg-gray-600 text-neutral-100'>
+      <div className='container mx-auto flex items-center justify-between py-4'>
+        <Link href='/'>Home</Link>
+        <div>
+          {userId ? (
+            <div className='flex gap-4 items-center'>
+              <Link href='/dashboard'>Dashboard</Link>
+              <UserButton afterSignOutUrl='/' />
+            </div>
+          ) : (
+            <div className='flex gap-4 items-center'>
+              <Link href='/sign-up'>Sign up</Link>
+              <Link href='/sign-in'>Sign In</Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
